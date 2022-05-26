173293de5945267d1ed0acc3046e804ce9650ab7
Aaron Guzman <aarongzmn@gmail.com>
2022-05-26 15:59:18 -0700

    updated documentation

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # listrak-api-wrapper
-This is a wrapper for the Listrak API. This will primarily be a wrapper for the /email and /data endpoints
+This is a wrapper for the Listrak API. This will primarily be a wrapper for the /email
 - https://api.listrak.com/email (work in progress)
-- https://api.listrak.com/data (not started)
--- listrak/__init__.py
+++ listrak/__init__.py
@@ -1,36 +1,25 @@
 import requests
 from datetime import datetime, timedelta
 
-from . import email
+from listrak.lib import email
 
 
 class Listrak:
     """https://api.listrak.com/email
     """
     def __init__(self, client_id, client_secret):
-        self._root_endpoint = "https://api.listrak.com"
         self._client_id = client_id
         self._client_secret = client_secret
 
+        self._root_endpoint = "https://api.listrak.com"
+        self._session = requests.Session()
         self._get_auth_token_data()
 
-        self.uri_list = email.ListEndpoint(self._root_endpoint, self._bearer_token)
-        self.uri_list_import = email.ListImportEndpoint(self._root_endpoint, self._bearer_token)
-        self.uri_contact = email.ContactEndpoint(self._root_endpoint, self._bearer_token)
-        self.uri_segmentation_field = email.SegmentationFieldEndpoint(self._root_endpoint, self._bearer_token)
-        self.uri_segmentation_field_group = email.SegmentationFieldGroupEndpoint(self._root_endpoint, self._bearer_token)
-
-    def get_auth_token_data(self) -> None:
-        """Authenticate using OAuth 2.0.
-        Helper function used to check if bearer token has expired.
-        If the bearer token has expired, it will be replaced by a new one.
-        """
-        if datetime.now() >= self._auth_expire_dt:
-            print("Listrak API token expired, getting new token.")
-            self._get_auth_token_data()
-            return
-        else:
-            return
+        self.uri_list = email.ListEndpoint(self._root_endpoint, self._session)
+        self.uri_list_import = email.ListImportEndpoint(self._root_endpoint, self._session)
+        self.uri_contact = email.ContactEndpoint(self._root_endpoint, self._session)
+        self.uri_segmentation_field = email.SegmentationFieldEndpoint(self._root_endpoint, self._session)
+        self.uri_segmentation_field_group = email.SegmentationFieldGroupEndpoint(self._root_endpoint, self._session)
 
     def _get_auth_token_data(self) -> None:
         """Authenticate using OAuth 2.0.
@@ -46,10 +35,8 @@ class Listrak:
         r = requests.post("https://auth.listrak.com/OAuth2/Token", data=body)
         try:
             r.raise_for_status()
-            auth_token_data = r.json()
-            self._bearer_token = auth_token_data["access_token"]
-            expires_in = auth_token_data["expires_in"]
-            self._auth_expire_dt = datetime.now() + timedelta(seconds=expires_in)
+            self._session.headers.update({"Authorization": f"Bearer {r.json()['access_token']}"})
+            self._auth_expire_dt = datetime.now() + timedelta(seconds=r.json()["expires_in"])
         except requests.exceptions.HTTPError as e:
             print(e)
         return
